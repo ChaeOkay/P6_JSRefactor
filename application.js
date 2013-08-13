3eac3eab852bbb5ba1c67643e6fb60aba64b6602
cokeefe <chaemobile@gmail.com>
2013-08-12 21:05:22 -0700

    annon funcitons progress

--- application.js
+++ application.js
@@ -1,19 +1,37 @@
 jQuery(function($) {
 
-  function addDie() {
-    $('.dice').append('<div class="die">0</div>');
+  function Die() {
+      this.addDie = function() {
+        $('.dice').append('<div class="die">0</div>');
+      };
+
+      this.rollDie = function() {
+        $('.die').each(function(k, die) {
+        var value = Math.floor((Math.random()*6)+1);
+        $(die).text(value);
+        });
+      };
   };
 
-  function rollDie() {
-    $('.die').each(function(k, die) {
-    var value = Math.floor((Math.random()*6)+1);
-    $(die).text(value);
-    });
-  };
+  var game = new Die();
 
-  $(document).ready(function() {
-    $('#roller button.add').on('click', addDie);
-    $('#roller button.roll').on('click', rollDie);
-  });
+  $('#roller button.add').on('click', game.addDie());
+  $('#roller button.roll').on('click', game.rollDie());
+
+  // function addDie() {
+  //   $('.dice').append('<div class="die">0</div>');
+  // };
+
+  // function rollDie() {
+  //   $('.die').each(function(k, die) {
+  //   var value = Math.floor((Math.random()*6)+1);
+  //   $(die).text(value);
+  //   });
+  // };
+
+  // $(document).ready(function() {
+  //   $('#roller button.add').on('click', addDie);
+  //   $('#roller button.roll').on('click', rollDie);
+  // });
 
 });
